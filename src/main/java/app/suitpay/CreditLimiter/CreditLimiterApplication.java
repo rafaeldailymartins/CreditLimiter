5fc8428bb16f403d2751053c3893aeaf4e4bf4ae
Rafael Daily <rafael@pilotar.app>
2025-09-23 12:03:16 -0300

    chore: add intial files

---
+++ src/main/java/app/suitpay/CreditLimiter/CreditLimiterApplication.java
@@ -0,0 +1,13 @@
+package app.suitpay.CreditLimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/suitpay/CreditLimiter/CreditLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package app.suitpay.CreditLimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
